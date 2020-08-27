11562fa82877dfe1296cdb458fb8722eba5241b3
Atineon <41876775+Atineon@users.noreply.github.com>
2020-08-27 16:46:18 +0200

    Add files via upload

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -1,17 +1,16 @@
 const Discord = require("discord.js");
-const {version} = require("../config.json");
+const {creator, version} = require("../config.json");
 
 
 module.exports = {
     name: "help",
     execute(message, args) {
         var embed = new Discord.MessageEmbed()
-            .setAuthor("Atineon#2652", "https://i.imgur.com/piiVdot.png")
             .setThumbnail("https://i.imgur.com/mWhU0Lh.png")
             .setTitle("TempChannels")
             .setDescription("Create your own temporary channels.")
             .addField("Support", "If you have something to ask or feedback, go on the support server of the bot [here](https://discord.gg/F3GDHhM).")
-            .setFooter(`version : ${version}`)
+            .setFooter(`created by : ${creator} | version : ${version}`)
             .setTimestamp();
         message.channel.send(embed)
     }
--- .gitignore/index.js
+++ .gitignore/index.js
@@ -67,4 +67,4 @@ client.on("message", message => {
     }
 });
 
-client.login(process.env.TOKEN);
+client.login(token);
