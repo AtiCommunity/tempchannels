592b86c8c1320bf2a516074cedd4c7bf43123f37
Atineon <41876775+Atineon@users.noreply.github.com>
2020-08-27 22:01:38 +0200

    Add files via upload

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const {creator, version} = require("../config.json");
+const {creators, version} = require("../config.json");
 
 
 module.exports = {
@@ -9,8 +9,9 @@ module.exports = {
             .setThumbnail("https://i.imgur.com/mWhU0Lh.png")
             .setTitle("TempChannels")
             .setDescription("Create your own temporary channels.")
+            .addField("How to use", "To use it correcly, you just need to create a voice channel with this name \"[TC] NVC\", then join it and it's done :wink:")
             .addField("Support", "If you have something to ask or feedback, go on the support server of the bot [here](https://discord.gg/F3GDHhM).")
-            .setFooter(`created by : ${creator} | version : ${version}`)
+            .setFooter(`created by : ${creators} | version : ${version}`)
             .setTimestamp();
         message.channel.send(embed)
     }
--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,24 +3,23 @@
 ///////////////////
 
 const Discord = require("discord.js");
-const fs = require("fs");
-const {prefix} = require("./config.json");
-
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const fs = require("fs");
+const {autochannel_name, prefix} = require("./config.json");
 
+client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
-}
+};
 
 
 client.on("ready", () => {
     const status = [
-        `${client.guilds.cache.size} servers | tc/create`,
-        `${client.channels.cache.size} channels | tc/create`,
-        `${client.users.cache.size} users | tc/create`,
+        `${client.guilds.cache.size} servers | tc/help`,
+        `${client.channels.cache.size} channels | tc/help`,
+        `${client.users.cache.size} users | tc/help`,
     ];
     var x = 0;
 
@@ -31,40 +30,64 @@ client.on("ready", () => {
         if(x == 3){
             x = 0;
         }
-    }, 30000)
-    console.log(`Bot: Online on ${client.guilds.cache.size} servers`);
-});
-
-
-client.on("guildMemberAdd", member => {
-    var autorole = member.guild.roles.cache.find(role => role.name === "TempChannels - off");
-
-
-    member.roles.add(autorole);
-    console.log(`Bot: New role assigned to ${member.user.username}.`)
-});
+    }, 30000);
+    console.log(`INFO: Online on ${client.guilds.cache.size} servers`);
+})
 
 
 client.on("message", message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-
-
-    if(message.author.bot) return
-
-    if(!message.content.startsWith(prefix) || message.author.bot) return
-
-    if(!client.commands.has(command)) return
-
     
+
+    if(message.author.bot){
+        return;
+    }
+
+    if(!message.content.startsWith(prefix) || message.author.bot){
+        return;
+    }
+
+    if(!client.commands.has(command)){
+        return;
+    }
+
     try{
         client.commands.get(command).execute(message, args);
     }
 
     catch(error){
-        console.error(error);
         message.channel.send("Error : The command was not executed successfully.");
+        console.log(`WARNING: ${message.author.username} have found a bug in the system !!! (${command} command)`);
+        console.error(error);
     }
-});
+})
+
+client.on("voiceStateUpdate", (oldState, newState) => {
+    global.joined_user = newState.member;
+    global.new_voicechannel_name = newState.channel && newState.channel.name;
+    var joined_username = newState.member.user.username;
+    var old_voicechannel_name = oldState.channel && oldState.channel.name;
+    var server = newState.guild.name;
+
+
+    if(new_voicechannel_name == autochannel_name){
+        newState.guild.channels.create(joined_username, {type: "voice"});
+        console.log(`INFO: ${joined_username} created a channel on ${server}.`);
+    }
+    if(old_voicechannel_name == oldState.member.user.username){
+        client.channels.cache.find(channel => channel.name === oldState.member.user.username).delete();
+        console.log(`INFO: ${joined_username} deleted a channel on ${server}.`);
+    }
+})
+
+client.on("channelCreate", channel => {
+    var created_channel = channel.id;
+
+
+    if(new_voicechannel_name == autochannel_name){
+        joined_user.voice.setChannel(created_channel);
+    }
+})
 
 client.login(process.env.TOKEN);
