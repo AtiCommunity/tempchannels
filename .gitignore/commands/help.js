66f6f0d168e2ae2eb4997534d56a0c1728866b09
Atineon <41876775+Atineon@users.noreply.github.com>
2020-08-27 22:24:03 +0200

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
             .setThumbnail("https://i.imgur.com/mWhU0Lh.png")
             .setTitle("TempChannels")
             .setDescription("Create your own temporary channels.")
-            .addField("How to use", "To use it correcly, you just need to create a voice channel with this name \"[TC] NVC\", then join it and it's done :wink:")
+            .addField("How to use", "To use it correcly, you just need to create a voice channel with this name \"[TC] New Voice Chennel\", then join it and it's done :wink:")
             .addField("Support", "If you have something to ask or feedback, go on the support server of the bot [here](https://discord.gg/F3GDHhM).")
             .setFooter(`created by : ${creators} | version : ${version}`)
             .setTimestamp();
