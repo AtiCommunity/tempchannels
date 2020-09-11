c6de7a2207ffa1b31e716fa72cb9d99c6e02d324
Atineon <41876775+Atineon@users.noreply.github.com>
2020-09-12 01:52:10 +0200

    Add files via upload

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
             .setThumbnail("https://i.imgur.com/mWhU0Lh.png")
             .setTitle("TempChannels")
             .setDescription("Create your own temporary channels.")
-            .addField("How to use", `To use it correcly, you just need to enter the command \"tc/help"\ and join the ${autochannel_name} channel and it's done :wink:`)
+            .addField("How to use", `To use it correcly, you just need to enter the command \"tc/init"\ and join the ${autochannel_name} channel and it's done :wink:`)
             .addField("Support", "If you have something to ask or feedback, go on the support server of the bot [here](https://discord.gg/F3GDHhM).")
             .setFooter(`created by : ${creators} | version : ${version}`)
             .setTimestamp();
