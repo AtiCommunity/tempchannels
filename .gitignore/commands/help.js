6f0afb070fc206518588a19838ade1d9a507da86
Atineon <41876775+Atineon@users.noreply.github.com>
2020-12-03 21:46:51 +0100

    Add files via upload

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -9,6 +9,7 @@ module.exports = {
             .setThumbnail("https://i.imgur.com/mWhU0Lh.png")
             .setTitle("TempChannels")
             .setDescription("Create your own temporary channels.")
+            .addField("Discord invite", `[Discord invite](https://discord.com/api/oauth2/authorize?client_id=731980097946517675&permissions=17001552&scope=bot)`)
             .addField("How to use", `To use it correcly, you just need to enter the command \"tc/init"\ and join the ${autochannel_ncchannel} channel and it's done :wink:`)
             .addField("Support", "If you have something to ask or feedback, go on the support server of the bot [here](https://discord.gg/F3GDHhM).")
             .setFooter(`created by : ${creators} | version : ${version}`)
