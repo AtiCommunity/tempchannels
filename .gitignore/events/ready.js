69e6f5834b1c5d6eb542a7340364d6072b88b6fc
Atineon <41876775+Atineon@users.noreply.github.com>
2021-03-06 09:26:13 +0100

    Add files via upload

--- .gitignore/events/ready.js
+++ .gitignore/events/ready.js
@@ -1,3 +1,20 @@
 module.exports = (client) => {
+    const status = [
+        `${client.guilds.cache.size} servers | tc/help`,
+        `${client.channels.cache.size} channels | tc/help`,
+        `${client.users.cache.size} users | tc/help`
+    ];
+    var x = 0;
+
+
+    setInterval(() => {
+        client.user.setActivity(status[x], {type: "WATCHING"});
+        x = x+1;
+        if(x == 3){
+            x = 0;
+        }
+    }, 30000);
+    console.log(client.guilds.cache.map(r => `name : ${r.name} | members : ${r.memberCount}`));
+    console.log(`INFO: Online on ${client.guilds.cache.size} servers`);
     console.log(`Logged in as ${client.user.tag}!`);
 };
