2d88246280d2af4fe18cc231e7447a81daff115a
Antoine ROBIN <41876775+Atineon@users.noreply.github.com>
2021-08-06 17:53:10 +0200

    Add files via upload

--- .gitignore/events/message.js
+++ .gitignore/events/message.js
@@ -1,19 +1,29 @@
-const { PREFIX } = require("../config.json");
+const { COMMANDS, PREFIX } = require("../config.json");
 
 module.exports = (client, message) => {
     const args = message.content.slice(PREFIX.length).split(/ +/);
     const command = args.shift().toLowerCase();
+    var command_size = COMMANDS.length;
 
     if(!message.content.startsWith(PREFIX) || message.author.bot) return;
 
     console.log(`INFO: ${message.author.tag} used the command ${command}.`);
 
-    try{
-        client.commands.get(command).execute(message, args);
-    }
-
-    catch(error){
-        message.reply("The command was not executed successfully or the command is unknown.");
-        console.error(error);
+    for(var i = 0; i < command_size; i++){
+        if(command == COMMANDS[i]){           
+            try{
+                client.commands.get(command).execute(message, args);
+            }
+        
+            catch(error){
+                message.reply("The command was not executed successfully.");
+                console.error(error);
+            }
+            break;
+        }
+        else{
+            message.reply("unknown command.")
+            break;
+        }
     }
 };
