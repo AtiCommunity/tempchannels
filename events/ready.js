23bc745e8bcd0c0617982414aa9f418c04e3c053
Antoine ROBIN <41876775+Atineon@users.noreply.github.com>
2021-08-22 16:01:02 +0200

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -2,17 +2,26 @@ module.exports = (client) => {
     const status = [
         `${client.guilds.cache.size} servers | tc/help`,
         `${client.channels.cache.size} channels | tc/help`,
-        // `${client.users.cache.size} users | tc/help`
+        // `${client.users.cache.size} users | tc/help`,
+        `tc/update available`
     ];
     var x = 0;
 
 
     setInterval(() => {
-        if(x == 2){ // default = 3
+        console.log(x + " " + status[x]);
+        if(x <= 1){
+            client.user.setActivity(status[x], {type: "WATCHING"});
+        }
+        else{
+            client.user.setActivity(status[x], {type: "PLAYING"});
+        }
+        if(x == 2){
             x = 0;
         }
-        client.user.setActivity(status[x], {type: "WATCHING"});
-        x++;
+        else{
+            x++;
+        }
     }, 10000);
     console.log(client.guilds.cache.map(guild => `name : ${guild.name} | members : ${guild.memberCount}`));
     console.log(`INFO: Online on ${client.guilds.cache.size} servers.`);
