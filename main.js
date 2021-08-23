6a6e250345a9def627b6141ec50a5cfd398c8786
Antoine ROBIN <41876775+Atineon@users.noreply.github.com>
2021-08-23 15:24:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_VOICE_STATES
   ]
