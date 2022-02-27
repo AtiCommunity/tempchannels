4eaccd03af03418fb3ac6804efa49c618230112c
Antoine ROBIN <41876775+Atineon@users.noreply.github.com>
2022-02-27 01:13:29 +0100

    Update update.js

--- src/commands/update.js
+++ src/commands/update.js
@@ -9,10 +9,10 @@ module.exports = {
             .setThumbnail("https://i.imgur.com/Mx2FDp2.png")
             .addFields(
                 {name: "**Latest update [22.02.27]**", value: `- Fix bot crash when a user leave channels.`},
+                {name: "\u200B", value: "\u200B"},
                 {name: "**Update [21.08.23]**", value: `- Help command has been updated again with one more information about source code.
                                                         - Update command has been updated with a space to see the latest update even better.
                                                         - A new category calculator in the bot activity. :wink:`},
-                {name: "\u200B", value: "\u200B"},
                 {name: "**Update [21.08.22]**", value: `- Help command has been updated with more informations about the bot.
                                                         - tc/update added to see update notes.`},
                 {name: "**Update [21.08.21]**", value: `- Reduced the time at 1000ms to create a temporary channel to have more chance to be moved in the channel if you are lagging.
