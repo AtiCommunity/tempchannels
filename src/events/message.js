74692b22010e4dfc34dfc7da6721f1a586c9cca2
Antoine ROBIN <41876775+Atineon@users.noreply.github.com>
2022-02-27 01:03:25 +0100

    Add files via upload

---
+++ src/events/message.js
@@ -0,0 +1,40 @@
+const { COMMANDS, PREFIX } = require("../config.json");
+
+module.exports = (client, message) => {
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+    console.log(`INFO: ${message.author.tag} used the command ${command}.`);
+    
+    identification(command);
+
+    function identification(command) {
+        var identification;
+        for(var i=0; i<COMMANDS.length; i++){
+            if(command == COMMANDS[i]){
+                identification = true;
+                break;
+            }
+            else{
+                identification = false;
+            }
+        }
+        if(identification == true){
+            try{
+                client.commands.get(command).execute(message, args);
+            }
+        
+            catch(error){
+                message.reply("The command was not executed successfully.");
+                console.error(error);
+            }
+        }
+        else{
+            message.reply("Unknow command.")
+        }
+    }
+};
+
+
